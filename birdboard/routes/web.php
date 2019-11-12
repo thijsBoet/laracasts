a2fae03b5c48c0f3f9b5f8461e7e9a2a47b59ee6
debian <debian@debian.HOMEGROUP>
2019-11-12 19:33:30 +0100

    finished model tests

--- birdboard/routes/web.php
+++ birdboard/routes/web.php
@@ -15,5 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/projects', 'ProjectsController@index');
-Route::post('/projects', 'ProjectsController@store');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/projects', 'ProjectsController@index');
+    Route::get('/projects/{project}', 'ProjectsController@show');
+    Route::post('/projects', 'ProjectsController@store');
+
+    Route::get('/home', 'HomeController@index')->name('home');
+});
+
+
+Auth::routes();
+
+
--- birdboard/tests/Feature/ProjectsTest.php
+++ birdboard/tests/Feature/ProjectsTest.php
@@ -12,9 +12,35 @@ class ProjectsTest extends TestCase
 
 
     /** @test */
+    public function guests_cannot_create_projects()
+    {   
+        $attributes = factory('App\Project')->raw();
+
+        $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+
+    /** @test */
+    public function guests_cannot_view_projects()
+    {
+        // $this->withoutExceptionHandling();
+
+        $this->get('/projects')->assertRedirect('login');
+    }
+
+    /** @test */
+    public function guests_cannot_view_a_single_projects()
+    {
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertRedirect('login');
+    }
+
+    /** @test */
     public function a_user_can_create_a_project()
     {
         $this->withoutExceptionHandling();
+
+        $this->actingAs(factory('App\User')->create());
 
         $attributes = [
             'title' => $this->faker->sentence,
@@ -30,8 +56,34 @@ class ProjectsTest extends TestCase
     }
 
     /** @test */
+    public function a_user_can_view_their_project()
+    {
+        $this->be(factory('App\User')->create());
+
+        $this->withoutExceptionHandling();
+        
+        $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
+
+        $this->get($project->path())
+            ->assertSee($project->title)
+            ->assertSee($project->description);
+    }
+
+    /** @test */
+    public function an_authenticated_user_cannot_view_the_projects_of_others()
+    {
+        $this->be(factory('App\User')->create());
+        
+        $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
+
+        $this->get($project->path())->assertStatus(403);
+    }
+
+    /** @test */
     public function a_project_requires_a_title()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['title' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
@@ -40,6 +92,8 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_description()
     {
+        $this->actingAs(factory('App\User')->create());
+        
         $attributes = factory('App\Project')->raw(['description' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
